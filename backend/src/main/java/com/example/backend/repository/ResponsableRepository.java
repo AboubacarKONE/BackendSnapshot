ed336eee1c3ff6ec6cd5ba3a2f5c29259feabbef
Mohamed Lamine Koné <mohamedlamine.kone@odc.local>
2021-12-03 12:19:16 +0000

    Exception mise a jour N°2

--- backend/src/main/java/com/example/backend/repository/ResponsableRepository.java
+++ backend/src/main/java/com/example/backend/repository/ResponsableRepository.java
@@ -1,10 +1,19 @@
 package com.example.backend.repository;
 
+import com.example.backend.model.Participant;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.example.backend.model.Responsable;
+
+import java.util.Optional;
+
 @CrossOrigin("*")
 public interface ResponsableRepository extends JpaRepositoryImplementation<Responsable, Long> {
+
+    @Query("SELECT p FROM Responsable p WHERE p.email = :email")
+    Optional<Responsable> findResponsable(@Param("email") String email);
 
 }
